8799e3cb1a8d83a812ed9590d806ba0d7ded95f9
mariana <marianalais.eng@gmail.com>
2023-06-17 14:27:11 -0300

    ajustes css table e home

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,5 +1,5 @@
 import { iProduct } from '../../types/product';
-import './styles.css';
+import styles from './styles.module.scss';
 import Table from '@mui/material/Table';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
@@ -9,16 +9,30 @@ import Paper from '@mui/material/Paper';
 
 export default function TableComponent({ lista }: { lista: iProduct[] }) {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
+    <TableContainer component={Paper} className={styles.tableContainer}>
+      <Table
+        className={styles.table}
+        sx={{ minWidth: 650 }}
+        aria-label="simple table"
+      >
+        <TableHead className={styles.tableHead}>
           <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell align="right">Produto</TableCell>
-            <TableCell align="right">Marca</TableCell>
-            <TableCell align="right">Categoria</TableCell>
-            <TableCell align="right">Estoque</TableCell>
-            <TableCell align="right">Valor</TableCell>
+            <TableCell className={styles.colum1}>ID</TableCell>
+            <TableCell className={styles.colum2} align="right">
+              Produto
+            </TableCell>
+            <TableCell className={styles.colum3} align="right">
+              Marca
+            </TableCell>
+            <TableCell className={styles.colum4} align="right">
+              Categoria
+            </TableCell>
+            <TableCell className={styles.colum5} align="right">
+              Estoque
+            </TableCell>
+            <TableCell className={styles.colum6} align="right">
+              Valor
+            </TableCell>
           </TableRow>
         </TableHead>
 
@@ -28,15 +42,29 @@ export default function TableComponent({ lista }: { lista: iProduct[] }) {
               key={product.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell align="right">{product.id}</TableCell>
+              <TableCell className={styles.tableCell} align="right">
+                {product.id}
+              </TableCell>
 
-              <TableCell component="th" scope="row">
+              <TableCell
+                className={styles.tableCell}
+                component="th"
+                scope="row"
+              >
                 {product.name}
               </TableCell>
-              <TableCell align="right">{product.brand}</TableCell>
-              <TableCell align="right">{product.category}</TableCell>
-              <TableCell align="right">{product.quantity}</TableCell>
-              <TableCell align="right">{product.sellPrice}</TableCell>
+              <TableCell className={styles.tableCell} align="right">
+                {product.brand}
+              </TableCell>
+              <TableCell className={styles.tableCell} align="right">
+                {product.category}
+              </TableCell>
+              <TableCell className={styles.tableCell} align="right">
+                {product.quantity}
+              </TableCell>
+              <TableCell className={styles.tableCell} align="right">
+                {product.sellPrice}
+              </TableCell>
             </TableRow>
           ))}
       </Table>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './styles.css'; // Arquivo CSS para estilização da tabela
+import styles from './styles.module.scss';
 import { iProduct } from '../../types/product';
 import TableComponent from '../../components/Table';
 import Button from '@mui/material/Button';
@@ -146,16 +146,21 @@ export default function Home() {
     <div>
       <Header />
       <TableComponent lista={listaProducts} />
-      <Button variant="contained" onClick={() => setOpenModalNewProduct(true)}>
-        Adicionar Produto
-      </Button>
-      <Button
-        variant="contained"
-        color="success"
-        onClick={() => setSellProduct(true)}
-      >
-        Vender Produto
-      </Button>
+      <div className={styles.content}>
+        <Button
+          variant="contained"
+          onClick={() => setOpenModalNewProduct(true)}
+        >
+          Adicionar Produto
+        </Button>
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() => setSellProduct(true)}
+        >
+          Vender Produto
+        </Button>
+      </div>
     </div>
   );
 }
