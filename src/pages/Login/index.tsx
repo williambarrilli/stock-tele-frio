9cf5231776a51ad60ba94af647e031dac397cd9d
mariana <marianalais.eng@gmail.com>
2023-06-22 14:50:36 -0300

    create page login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,39 @@
-import React from "react";
+import styles from './styles.module.scss';
+import Input from '../../components/InputComponent/index';
 
-export default function Login() {
-  return <div>Login</div>;
+interface LoginProps {
+  login: string;
+  senha: string;
+  setLogin: (value: string) => void;
+  setSenha: (value: string) => void;
+}
+export default function Login({
+  login,
+  setLogin,
+  senha,
+  setSenha,
+}: LoginProps) {
+  const senhaChange = (value: any) => {
+    setSenha(value);
+  };
+  return (
+    <div className={styles.container}>
+      <div>
+        <Input
+          type="text"
+          value={login}
+          placeholder="Digite seu login"
+          label="Login:"
+          onChange={(value) => setLogin(value)}
+        />
+        <Input
+          type="text"
+          value={senha}
+          placeholder="Digite sua senha"
+          label="Senha:"
+          onChange={senhaChange}
+        />
+      </div>
+    </div>
+  );
 }
